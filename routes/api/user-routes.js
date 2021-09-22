25d7fc6642e653e417fb964e9e8229fe40533f0c
Aidan Amato <aidanamato@comcast.net>
2021-09-22 13:57:10 -0400

    add user credential validation

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -45,6 +45,28 @@ router.post('/', ({body}, res) => {
     });
 });
 
+//POST /api/users/login
+router.post('/login', ({body}, res) => {
+  User.findOne({
+    where: {
+      email: body.email
+    }
+  }).then(dbUserData => {
+    if (!dbUserData) {
+      res.status(400).json({message: 'No user with that email address.'});
+      return;
+    }
+    
+    const validPassword = dbUserData.checkPassword(body.password);
+    if (!validPassword) {
+      res.status(400).json({message: 'Incorrect password'});
+      return;
+    }
+
+    res.json({user: dbUserData, message: 'You are now logged in!'});
+  });
+});
+
 // PUT /api/users/:id
 router.put('/:id', ({body, params}, res) => {
   User.update(body, {
